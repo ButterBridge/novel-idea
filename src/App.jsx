d4c2f086088d354e2e0c4a3b354409086d5a4e96
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-09-06 22:32:50 +0100

    set up base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import Group from './components/Group';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        My New App!
-      </div>
+      <BrowserRouter>
+        <div>
+          <h1>Novel Idea</h1>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route
+              path="/group/:groupId"
+              render={props => <Group {...props} />}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
